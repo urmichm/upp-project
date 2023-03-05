ef6e802c950dc3b826c5dfb807b780b9d4e4e6b8
Michael <urmich.m@gmail.com>
2023-03-05 10:39:11 +0100

    polynomial init

---
+++ upp/polynomial.h
@@ -0,0 +1,19 @@
+//
+//  polynomial.h
+//  upp
+//
+//  Created by Mikhail Urmich on 3/5/23.
+//
+
+#ifndef polynomial_h
+#define polynomial_h
+
+namespace upp {
+
+class polynomial {
+    
+};
+
+} // end namespace upp
+
+#endif /* polynomial_h */
